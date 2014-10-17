b4697a5e0674b871728ac1e84080a9533ffa61a5
Fabio Falci <fabiofalci@gmail.com>
2014-10-17 09:17:45 +0100

    Some cruzeira classes refactored

--- cruzeira-simple/src/main/java/controllers/Hello.java
+++ cruzeira-simple/src/main/java/controllers/Hello.java
@@ -4,12 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.concurrent.Callable;
+
 @Controller
 public class Hello {
 
-	@RequestMapping
-	public @ResponseBody String hello() {
-		return "Hello!!!";
-	}
+    @RequestMapping
+    public @ResponseBody String hello() {
+        return "Hello!!!";
+    }
+
+    @RequestMapping("/async")
+    public @ResponseBody Callable<String> asyncSimple() {
+        return new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return "Async Hello!!!";
+            }
+        };
+    }
 	
 }
